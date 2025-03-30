0674805c59bd3b88568393470956026c426e0dfe
blaack007 <mohamedkhalifa.4053@gmail.com>
2025-03-30 18:48:43 +0200

    Remove unused files and implement local storage for last search and theme settings

--- app.js
+++ app.js
@@ -1,24 +1,119 @@
-const API_KEY = "cb301aa38e9dbd9559f4f7bf62688659";
-const UNITS = "metric";
+// https://api.openweathermap.org/data/2.5/weather?q=Riyadh,sa&appid=505a3eec3e074cc74ceda32f6b638955&units=metric
 
-async function fetchWeatherData(country, city) {
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=${UNITS}`;
-  // get user email to set last searched country for specific user in localStorage
-  // using sessionStorage to save the data of the currently logged in user
-  //   const email = sessionStorage.getItem();
-  //   if (!email) {
-  //     alert("You're not logged in, Please login to proceed");
-  //     location.href = "login.html";
-  //   }
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-    const json = await response.json();
-    console.log(json);
-  } catch (error) {
-    console.error(error.message);
-  }
+const predefinedCities = [
+	{ city: "New York", country: "US" },
+	{ city: "Los Angeles", country: "US" },
+	{ city: "Chicago", country: "US" },
+	{ city: "London", country: "GB" },
+	{ city: "Paris", country: "FR" },
+	{ city: "Berlin", country: "DE" },
+	{ city: "Madrid", country: "ES" },
+	{ city: "Rome", country: "IT" },
+	{ city: "Amsterdam", country: "NL" },
+	{ city: "Moscow", country: "RU" },
+	{ city: "Beijing", country: "CN" },
+	{ city: "Shanghai", country: "CN" },
+	{ city: "Tokyo", country: "JP" },
+	{ city: "Seoul", country: "KR" },
+	{ city: "Bangkok", country: "TH" },
+	{ city: "Jakarta", country: "ID" },
+	{ city: "Mumbai", country: "IN" },
+	{ city: "New Delhi", country: "IN" },
+	{ city: "Dubai", country: "AE" },
+	{ city: "Riyadh", country: "SA" },
+	{ city: "Istanbul", country: "TR" },
+	{ city: "Cairo", country: "EG" },
+	{ city: "Johannesburg", country: "ZA" },
+	{ city: "Sydney", country: "AU" },
+	{ city: "Melbourne", country: "AU" },
+	{ city: "São Paulo", country: "BR" },
+	{ city: "Buenos Aires", country: "AR" },
+	{ city: "Mexico City", country: "MX" },
+	{ city: "Toronto", country: "CA" },
+	{ city: "Vancouver", country: "CA" },
+];
+
+// Initialize App
+(function initApp() {
+	console.log("initApp");
+	// Setup dropdown
+	createDropdown(predefinedCities);
+
+	// Set initial theme
+	const savedTheme = getTheme();
+	if (savedTheme) document.body.classList.add(savedTheme);
+
+	// Check for last search
+	const lastSearch = getLastSearch();
+	if (lastSearch) {
+		if (lastSearch.city) {
+			handleWeatherSearch(lastSearch.city, lastSearch.country);
+		}
+		if (lastSearch.lat && lastSearch.lon) {
+			handleWeatherSearch('',lastSearch.lat, lastSearch.lon);
+		}
+		
+	} else {
+		showGreeting();
+	}
+
+	setupEventListeners();
+})();
+
+function setupEventListeners() {
+	// Dropdown change
+	document.getElementById("city-select").addEventListener("change", (e) => {
+		const [city, country] = e.target.value.split(",");
+		if (city) handleWeatherSearch(city, country);
+	});
+
+	// Manual input with debounce
+	let timeout;
+	document.getElementById("manual-input").addEventListener("input", (e) => {
+		clearTimeout(timeout);
+		timeout = setTimeout(() => {
+			const city = e.target.value
+			if (city) handleWeatherSearch(city);
+		}, 3000);
+	});
+
+	// Theme toggle
+	document.getElementById("theme-toggle").addEventListener("click", () => {
+		const newTheme = toggleThemeUI();
+		saveTheme(newTheme);
+	});
+
+	document.getElementById("lcation-button").addEventListener("click", () => {
+	navigator.geolocation.getCurrentPosition(position => {
+		const lat = position.coords.latitude;
+		const lon = position.coords.longitude;
+		handleWeatherSearch('',lat, lon);
+
+		
+		
+	});
+	});
 }
-fetchWeatherData("Riyadh", "sa");
+
+async function handleWeatherSearch(city ='',lat = '', lon ='' ) {
+	try {
+		let data;
+		//get country code
+		if (city){
+			let country = await get_country_code(city);
+			data = await fetchWeather(city, country);
+			saveLastSearch(city);
+
+		}
+		else if (lat && lon){
+			data =  await  fetchWeather("","",lat, lon);
+			saveLastSearch("",lat, lon);
+		}
+		showWeather(data);
+		updateBackground(data.weather[0].main);
+	} catch (error) {
+		showError(error.message);
+	}
+}
+
+// storage functions
