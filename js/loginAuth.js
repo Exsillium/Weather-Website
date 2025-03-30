e1e1e9e0a78057a2785b66c78f96e9f9605125ea
Exsillium <marwan.tarek9980@gmail.com>
2025-03-30 16:30:54 +0200

    added more functionality to registeration

--- js/loginAuth.js
+++ js/loginAuth.js
@@ -1,8 +1,8 @@
+let users = getUsers();
 const loginForm = document.getElementById("login-form");
 
 loginForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const loginEmail = document.getElementById("login-email").value;
   const loginPassword = document.getElementById("login-password").value;
-  console.log(loginEmail, loginPassword);
 });
--- js/regAuth.js
+++ js/regAuth.js
@@ -1,8 +1,18 @@
+let users = getUsers();
 const registerForm = document.getElementById("register-form");
 
 registerForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const registerEmail = document.getElementById("register-email").value;
   const registerPassword = document.getElementById("register-password").value;
-  console.log(registerEmail, registerPassword);
+  if (!registerEmail || !registerPassword) {
+    alert("Enter a valid email and Password");
+    return;
+  }
+  if (alreadyRegistered(registerEmail)) {
+    alert("Email is already in use");
+    return;
+  }
+  addUser(registerEmail, registerPassword);
+  alert("Registered successfully!");
 });
